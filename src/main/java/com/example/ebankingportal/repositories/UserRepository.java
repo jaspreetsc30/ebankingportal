ca55a7b35af44437dc86a203f6ce2233a6ebb3f9
jaspreetsc30 <71060334+jaspreetsc30@users.noreply.github.com>
2023-04-02 07:33:22 +0800

    refactor: changed DB from mySQL to H2

--- src/main/java/com/example/ebankingportal/repositories/UserRepository.java
+++ src/main/java/com/example/ebankingportal/repositories/UserRepository.java
@@ -3,9 +3,6 @@ package com.example.ebankingportal.repositories;
 import com.example.ebankingportal.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByUserName(String userName);
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO USERS (id,iban, password, user_name) VALUES (1,'init', 'init', 'init');
